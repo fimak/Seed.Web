9e46ef3dfd61ceb626ac653cb32e27a062c5d237
Aleksandr Ufimtsev <fimak@bk.ru>
2024-02-14 22:42:01 +0700

    fix: styles

--- common/components/header/header.tsx
+++ common/components/header/header.tsx
@@ -1,6 +1,6 @@
 import BurgerMenu from "@components/header/burgerMenu";
 import { FlexibleLinkModel } from "@lib/umbraco/types/flexibleLinkModel.type";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./header.module.scss";
 import Image from "next/image";
 
@@ -16,9 +16,27 @@ export type HeaderModel = {
 };
 
 const Header: FC<HeaderModel> = ({ logo, mainLinks, secondaryLinks }) => {
+  const [opacity, setOpacity] = useState(0);
+
+  const handleScroll = () => {
+    if (window.scrollY <= 100) {
+      setOpacity(window.scrollY / 100);
+    }
+    console.log(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <header className={styles.header}>
-      <div className={styles.headerContainer}>
+      <div
+        className={styles.headerContainer}
+        style={{ backgroundColor: `rgba(0, 45, 86, ${opacity})` }}
+      >
         <Image src={logo} alt="logo" width={106} height={48} />
         <BurgerMenu mainLinks={mainLinks} secondaryLinks={secondaryLinks} />
       </div>
