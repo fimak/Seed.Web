425f1cc5ea81ac63a8d8674d66cc07fec4d4f613
Aleksandr Ufimtsev <fimak@bk.ru>
2024-02-14 08:39:14 +0700

    feat: implement mobile pinnedLinks

--- common/components/grid/controls/widgets/pinnedLinks/pinnedLinks.tsx
+++ common/components/grid/controls/widgets/pinnedLinks/pinnedLinks.tsx
@@ -1,12 +1,25 @@
-import { WidgetModel } from '@lib/umbraco/types/widgetModel.type';
-import styles from './pinnedLinks.module.scss';
-import WidgetWrapper from '../widgetWrapper';
+import { WidgetModel } from "@lib/umbraco/types/widgetModel.type";
+import Link from "next/link";
+import styles from "./pinnedLinks.module.scss";
+import WidgetWrapper from "../widgetWrapper";
+import Image from "next/image";
 
 export default function PinnedLinks(model: WidgetModel) {
-    
-    return (
-        <WidgetWrapper model={model} styles={styles}>
-            <p>Code goes here</p>
-        </WidgetWrapper>
-    )
+  return (
+    <WidgetWrapper model={model} styles={styles}>
+      <nav>
+        {model.content.links.map((link: NodeJS.Dict<any>) => (
+          <Link href={link.url} key={link.label}>
+            <Image
+              src={`/media/icons/${link.icon}.svg`}
+              alt={link.icon}
+              width={18}
+              height={18}
+            />
+            <span dangerouslySetInnerHTML={{ __html: link.label }} />
+          </Link>
+        ))}
+      </nav>
+    </WidgetWrapper>
+  );
 }
