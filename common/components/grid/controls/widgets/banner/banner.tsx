7fd6f3579647f59fc0d425106da5c167cc107a40
Aleksandr Ufimtsev <fimak@bk.ru>
2024-02-14 07:28:31 +0700

    feat: implement mobile banner

--- common/components/grid/controls/widgets/banner/banner.tsx
+++ common/components/grid/controls/widgets/banner/banner.tsx
@@ -1,16 +1,19 @@
-import { ImageModel } from "@lib/umbraco/types/imageModel.type";
 import { WidgetModel } from "@lib/umbraco/types/widgetModel.type";
-import { getCropUrl } from "@lib/umbraco/util/helpers";
-import Rte from "../../rte";
 import WidgetWrapper from "../widgetWrapper";
 import styles from "./banner.module.scss";
-import CroppedImage from "@components/images/croppedImage";
-import isDark from "common/util/isDark";
 
 export default function Banner(model: WidgetModel) {
-    return (
-        <WidgetWrapper model={model} styles={styles}>
-            <p>Markup goes here.</p>
-        </WidgetWrapper>
-    )
+  return (
+    <WidgetWrapper
+      model={model}
+      styles={styles}
+      style={{ backgroundImage: `url(${model.content.image})` }}
+      className={styles.widgetBanner}
+    >
+      <div
+        className={styles.text}
+        dangerouslySetInnerHTML={{ __html: model.content.text }}
+      />
+    </WidgetWrapper>
+  );
 }
