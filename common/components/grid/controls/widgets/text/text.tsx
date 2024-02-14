a9989cd3339994279c971d2c6e15dd172deda72d
Aleksandr Ufimtsev <fimak@bk.ru>
2024-02-14 09:15:46 +0700

    feat: implement mobile Text

--- common/components/grid/controls/widgets/text/text.tsx
+++ common/components/grid/controls/widgets/text/text.tsx
@@ -2,19 +2,29 @@ import { WidgetModel } from "@lib/umbraco/types/widgetModel.type";
 import isDark from "common/util/isDark";
 import Rte from "../../rte";
 import WidgetWrapper from "../widgetWrapper";
-import styles from './text.module.scss';
+import styles from "./text.module.scss";
 
 export type TextModel = {
-    copy: string,
-    backgroundColor?: string
-}
+  text: string;
+  backgroundColor?: string;
+};
 
 export default function Text(model: WidgetModel) {
-    const {copy, backgroundColor} = model.content as TextModel;
-    const dark = isDark(backgroundColor);
-    return (
-        <WidgetWrapper className={(dark ? 'darkBackground' : '') + (backgroundColor ? ' ' + styles.hasBackground: '')} model={model} styles={styles} style={{backgroundColor: backgroundColor ? '#' + backgroundColor : undefined}} >
-            <Rte className={styles.text} text={copy} />
-        </WidgetWrapper>
-    )
+  const { text, backgroundColor } = model.content as TextModel;
+  const dark = isDark(backgroundColor);
+  return (
+    <WidgetWrapper
+      className={
+        (dark ? "darkBackground" : "") +
+        (backgroundColor ? " " + styles.hasBackground : "")
+      }
+      model={model}
+      styles={styles}
+      style={{
+        backgroundColor: backgroundColor ? "#" + backgroundColor : undefined,
+      }}
+    >
+      <Rte className={styles.text} text={text} />
+    </WidgetWrapper>
+  );
 }
